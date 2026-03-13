e095436203fb3ede22326f3c1264700dd27a4ea6
luca7005 <luca7597@gmail.com>
2026-03-13 12:13:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Page() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Immobilienrechner</h1>
-      <p>Die Testseite läuft erfolgreich.</p>
+    <main style={{padding:40}}>
+      <h1>Immobilienrechner läuft ✅</h1>
     </main>
-  );
+  )
 }
